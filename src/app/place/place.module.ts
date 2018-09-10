a029023f9fc6067762fe5cbf0743db0761130c10
Oscar reyes <oscarreyes@Oscars-MacBook-Pro.local>
2018-09-09 18:56:31 -0600

    :construction: se creo componente de los lugares

--- src/app/place/place.module.ts
+++ src/app/place/place.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PlaceComponent } from './place/place.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [PlaceComponent]
 })
 export class PlaceModule { }
