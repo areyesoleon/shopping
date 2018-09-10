5763d73221cdd935a2d1fbad7864ffd71c20c2a2
Oscar reyes <oscarreyes@Oscars-MacBook-Pro.local>
2018-09-09 19:44:33 -0600

    :construction: Se creo componente de los productos

--- src/app/item/item.module.ts
+++ src/app/item/item.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ItemComponent]
 })
 export class ItemModule { }
